2bb50195ea9ba5217915e05ce820436d7b8f9e61
Ang Lu <luang007@163.com>
2024-07-28 22:42:24 +0800

    Add files via upload

--- premake5.lua
+++ premake5.lua
@@ -1,45 +1,121 @@
-project "GLFW"
-    kind "StaticLib"
-    language "C"
+workspace "SimpleEngine"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug", 
+        "Release",
+        "Dist"
+    }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "EngineCore/third_parties/GLFW/include"
+
+include "EngineCore/third_parties/GLFW"
+
+project "EngineCore"
+    location "EngineCore"
+    kind "SharedLib"
+    language "C++"
     
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-        "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
+    pchheader "sepch.h"
+    pchsource "EngineCore/src/sepch.cpp"
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
     }
-    
-	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
-        systemversion "latest"
+
+    includedirs
+    {
+        "%{prj.name}/third_parties/spdlog/include",
+        "%{prj.name}/src",
+        "%{IncludeDir.GLFW}"
+    }
+
+    links
+    {
+        "GLFW",
+        "opengl32.lib"
+    }
+
+
+    filter "system:windows"
+        cppdialect "c++17"
         staticruntime "On"
-        
-        files
+        systemversion "latest"
+
+        defines
         {
-            "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
+            "SE_BUILD_DLL",
+            "SE_PLATFORM_WINDOWS"
         }
 
-		defines 
-		{ 
-            "_GLFW_WIN32",
-            "_CRT_SECURE_NO_WARNINGS"
-		}
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+        defines "SE_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "SE_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "SE_DIST"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "EngineCore/third_parties/spdlog/include",
+        "EngineCore/src"
+    }
+
+    links
+    {
+        "EngineCore"
+    }
+
+    filter "system:windows"
+        cppdialect "c++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
+        {
+            "SE_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "SE_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "SE_RELEASE"
+        optimize "On"
+
+    filter "configurations:Dist"
+        defines "SE_DIST"
+        optimize "On"
